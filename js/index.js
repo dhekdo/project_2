850a8b146404db1d34fcc86d9ae790a57c2b2f89
dhekdo <daae0924@naver.com>
2022-12-04 14:39:10 +0900

    2022.12.04_2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,56 @@
 
-$('#allbt').click(function(){
+$('#allbt').hover(function(){
     $('.allmenu').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.allmenu').hide();
 });
-$('#fabricbt').click(function(){
+
+$('#fabricbt').hover(function(){
     $('.fabric').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.fabric').hide();
+});
+
+$('#flbt').hover(function(){
+    $('.flooring').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.flooring').hide();
+});
+
+$('#moodbt').hover(function(){
+    $('.mood').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.mood').hide();
+});
+
+$('#spabt').hover(function(){
+    $('.spa').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.spa').hide();
+});
+
+$('#designbt').hover(function(){
+    $('.design').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.design').hide();
+});
+
+$('#teabt').hover(function(){
+    $('.tea').show();
+    $('.defalt').hide();
+}, function(){
+    $('.defalt').show();
+    $('.tea').hide();
 });
